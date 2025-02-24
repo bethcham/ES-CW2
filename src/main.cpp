2229058f54a5c79c06517022ec65644e3c2a91d0
Beth <bethcham@gmail.com>
2025-02-24 21:12:54 +0000

    Lab 1 completed

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include <U8g2lib.h>
+#include <bitset>
+#include <STM32FreeRTOS.h>
 
 //Constants
   const uint32_t interval = 100; //Display update interval
@@ -35,6 +37,63 @@
 //Display driver object
 U8G2_SSD1305_128X32_ADAFRUIT_F_HW_I2C u8g2(U8G2_R0);
 
+volatile uint32_t currentStepSize = 0;
+
+struct {
+  std::bitset<32> inputs;  
+  } sysState;
+
+HardwareTimer sampleTimer(TIM1);
+
+constexpr float baseFreq = 440.0; // frequency for A
+constexpr float sampleFreq = 22000.0; // fs
+
+constexpr uint32_t stepSizes[12] = {
+  static_cast<uint32_t>((baseFreq * pow(2.0, -9.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // C
+  static_cast<uint32_t>((baseFreq * pow(2.0, -8.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // C#
+  static_cast<uint32_t>((baseFreq * pow(2.0, -7.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // D
+  static_cast<uint32_t>((baseFreq * pow(2.0, -6.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // D#
+  static_cast<uint32_t>((baseFreq * pow(2.0, -5.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // E
+  static_cast<uint32_t>((baseFreq * pow(2.0, -4.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // F
+  static_cast<uint32_t>((baseFreq * pow(2.0, -3.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // F#
+  static_cast<uint32_t>((baseFreq * pow(2.0, -2.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // G
+  static_cast<uint32_t>((baseFreq * pow(2.0, -1.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq), // G#
+  static_cast<uint32_t>((baseFreq * (uint64_t(1) << 32)) / sampleFreq),                         // A
+  static_cast<uint32_t>((baseFreq * pow(2.0, 1.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq),  // A#
+  static_cast<uint32_t>((baseFreq * pow(2.0, 2.0 / 12.0) * (uint64_t(1) << 32)) / sampleFreq)   // B
+};
+
+const char* notes[12] = {
+  "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"
+};
+
+std::bitset<4> readCols(){
+  std::bitset<4> result;
+  result[0] = digitalRead(C0_PIN);
+  result[1] = digitalRead(C1_PIN);
+  result[2] = digitalRead(C2_PIN);
+  result[3] = digitalRead(C3_PIN);
+
+  return result;
+}
+
+void setRow(uint8_t rowIdx){
+  digitalWrite(RA0_PIN, rowIdx & 0x01);
+  digitalWrite(RA1_PIN, rowIdx & 0x02);
+  digitalWrite(RA2_PIN, rowIdx & 0x04);
+
+  digitalWrite(REN_PIN, HIGH);
+}
+
+void sampleISR(){
+  static uint32_t phaseAcc = 0;
+  uint32_t localCurrentStepSize = __atomic_load_n(&currentStepSize, __ATOMIC_RELAXED);
+  phaseAcc += localCurrentStepSize;
+
+  int32_t Vout = (phaseAcc >> 24) - 128;
+  analogWrite(OUTR_PIN, Vout + 128);
+}
+
 //Function to set outputs using key matrix
 void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,LOW);
@@ -45,6 +104,63 @@ void setOutMuxBit(const uint8_t bitIdx, const bool value) {
       digitalWrite(REN_PIN,HIGH);
       delayMicroseconds(2);
       digitalWrite(REN_PIN,LOW);
+}
+
+void scanKeysTask(void * pvParameters) {
+  const TickType_t xFrequency = 50/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while(1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    sysState.inputs.reset();
+
+    for (uint8_t i = 0; i < 3; i++){
+      setRow(i);
+      delayMicroseconds(3);
+      std::bitset<4> cols = readCols();
+      sysState.inputs |= (cols.to_ulong() << (i*4));
+    }
+
+    bool keyPressed = false;
+    uint32_t localCurrentStepSize = 0;
+
+    for (uint8_t i = 0; i < 12; i++) {
+      if (!sysState.inputs[i]) {
+        localCurrentStepSize = stepSizes[i];
+        keyPressed = true;
+        u8g2.setCursor(2,30);
+        u8g2.print(notes[i]);
+        break; 
+      }
+    }
+
+    if (!keyPressed) {
+      localCurrentStepSize = 0; 
+      u8g2.setCursor(2,30);
+      u8g2.print("-");
+    }
+
+    __atomic_store_n(&currentStepSize, localCurrentStepSize, __ATOMIC_RELAXED);
+  }
+}
+
+void displayUpdateTask(void * pvParameters){
+  const TickType_t xFrequency = 100/portTICK_PERIOD_MS;
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+
+  while(1) {
+    vTaskDelayUntil( &xLastWakeTime, xFrequency );
+    u8g2.clearBuffer();         // clear the internal memory
+    u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
+    u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
+    
+    u8g2.setCursor(2,20);
+    u8g2.print(sysState.inputs.to_ulong(), HEX);
+
+    u8g2.sendBuffer();          // transfer internal memory to the display
+    
+    digitalToggle(LED_BUILTIN);
+  }
 }
 
 void setup() {
@@ -67,6 +183,10 @@ void setup() {
   pinMode(JOYX_PIN, INPUT);
   pinMode(JOYY_PIN, INPUT);
 
+  sampleTimer.setOverflow(22000, HERTZ_FORMAT);
+  sampleTimer.attachInterrupt(sampleISR);
+  sampleTimer.resume();
+
   //Initialise display
   setOutMuxBit(DRST_BIT, LOW);  //Assert display logic reset
   delayMicroseconds(2);
@@ -77,26 +197,30 @@ void setup() {
   //Initialise UART
   Serial.begin(9600);
   Serial.println("Hello World");
+
+  TaskHandle_t scanKeysHandle = NULL;
+
+  xTaskCreate(
+    scanKeysTask,		/* Function that implements the task */
+    "scanKeys",		/* Text name for the task */
+    64,      		/* Stack size in words, not bytes */
+    NULL,			/* Parameter passed into the task */
+    2,			/* Task priority */
+    &scanKeysHandle );	/* Pointer to store the task handle */
+
+  TaskHandle_t displayUpdateHandle = NULL;
+  xTaskCreate(
+    displayUpdateTask,
+    "displayUpdate",
+    256,
+    NULL,
+    1,
+    &displayUpdateHandle
+  );
+
+  vTaskStartScheduler();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  static uint32_t next = millis();
-  static uint32_t count = 0;
-
-  while (millis() < next);  //Wait for next interval
-
-  next += interval;
-
-  //Update display
-  u8g2.clearBuffer();         // clear the internal memory
-  u8g2.setFont(u8g2_font_ncenB08_tr); // choose a suitable font
-  u8g2.drawStr(2,10,"Helllo World!");  // write something to the internal memory
-  u8g2.setCursor(2,20);
-  u8g2.print(count++);
-  u8g2.sendBuffer();          // transfer internal memory to the display
-
-  //Toggle LED
-  digitalToggle(LED_BUILTIN);
-  
+  // put your main code here, to run repeatedly:  
 }
